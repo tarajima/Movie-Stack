68d5fe9f18222783e70a70c49f80afa94c312aa0
taranakajima <nakatarat@gmail.com>
2020-01-11 20:39:55 -0800

    Added nav links

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return(
-    <nav></nav>
+    <nav>
+      <ul>
+        <Link to="/"><li>Home</li></Link>
+        <Link to="/discover"><li>Discover</li></Link>
+        <Link to="/about"><li>About</li></Link>
+      </ul>
+    </nav>
   );
 }
 
