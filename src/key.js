1a370a707eba46af44a99915fc132e72a8668e6a
taranakajima <nakatarat@gmail.com>
2020-05-21 12:03:24 -0700

    Add API config

---
+++ src/key.js
@@ -0,0 +1,11 @@
+const keyConfigObject = {
+  params: {
+    api_key: 'e18501ca1916f9f0d8b11e6bd3be62e1'
+  }
+}
+
+export const keyObject = {
+  api_key: 'e18501ca1916f9f0d8b11e6bd3be62e1'
+}
+
+export default keyConfigObject;
